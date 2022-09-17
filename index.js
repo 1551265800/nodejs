07f4bcaea36426d406ed8cabb955b329373f874e
诗昂 <1551265800@qq.com>
2022-09-18 02:07:10 +0800

    index

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-var a=10;
-console.log(a);
+const fs = require("fs");
+fs.readFile("./me.txt","utf8",(err,str)=>{
+    //失败的结果
+    if (err) { //判断文件是否读取成功
+        return console.log(err);
+    }
+    //成功之后的结果
+    console.log(str);
+})
