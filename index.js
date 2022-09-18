4e69f884d6e9ed3bda8ab670bb910e3db8234eb4
诗昂 <1551265800@qq.com>
2022-09-18 12:23:22 +0800

    index

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const fs = require("fs");
-fs.readFile("./me.txt","utf8",(err,str)=>{
+fs.readFile("./me.txt", "utf8", (err, str) => {
     //失败的结果
     if (err) { //判断文件是否读取成功
         return console.log(err);
     }
     //成功之后的结果
-    console.log("输出的结果:"+str);
+    console.log("文件读取成功:" + str);
+})
+fs.writeFile("./write.txt", "Hello Node.js", (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("文件写入成功");
 })
